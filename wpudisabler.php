19c9cab505fc4bee2d8bb2afef362427ff33f222
Kevin <kevin@rocher-dev.com>
2017-06-04 22:16:11 +0200

    v 0.2.0
    * Add translation.
    * Disable plugin deactivation.
    * Disable WP API.

--- wpudisabler.php
+++ wpudisabler.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WPU Disabler
 Description: Disable WordPress features
-Version: 0.1.0
+Version: 0.2.0
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -16,11 +16,20 @@ class WPUDisabler {
     }
 
     public function plugins_loaded() {
+
+        load_plugin_textdomain('wpudisabler', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+
         if (apply_filters('wpudisabler__disable_author_page', false)) {
             $this->disable_author_page();
         }
         if (apply_filters('wpudisabler__disable_feeds', false)) {
             $this->disable_feeds();
+        }
+        if (apply_filters('wpudisabler__disable_plugin_deactivation', false)) {
+            $this->disable_plugin_deactivation();
+        }
+        if (apply_filters('wpudisabler__disable_wp_api', false)) {
+            $this->disable_wp_api();
         }
     }
 
@@ -61,7 +70,40 @@ class WPUDisabler {
     }
 
     public function disable_feed() {
-        wp_die(sprintf(__('No feed available, please visit our <a href="%s">homepage</a>!'), get_bloginfo('url')));
+        wp_die(sprintf(__('No feed available, please visit our <a href="%s">homepage</a>!', 'wpudisabler'), get_bloginfo('url')));
+    }
+
+    /* ----------------------------------------------------------
+      Disable plugin deactivation
+    ---------------------------------------------------------- */
+
+    public function disable_plugin_deactivation() {
+        add_filter('plugin_action_links', array(&$this, 'disable_plugin_deactivation__action'));
+    }
+
+    public function disable_plugin_deactivation__action($links) {
+        if (isset($links['deactivate'])) {
+            unset($links['deactivate']);
+        }
+        if (isset($links['activate'])) {
+            unset($links['activate']);
+        }
+        return $links;
+    }
+
+    /* ----------------------------------------------------------
+      Disable WP API
+    ---------------------------------------------------------- */
+
+    public function disable_wp_api() {
+
+        // WP-API version 1.x
+        add_filter('json_enabled', '__return_false');
+        add_filter('json_jsonp_enabled', '__return_false');
+
+        // WP-API version 2.x
+        add_filter('rest_enabled', '__return_false');
+        add_filter('rest_jsonp_enabled', '__return_false');
     }
 
 }
